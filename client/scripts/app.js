d8ab4069ae9c5fc1074502a1c64305076f880099
brannonlucas <brannonlucas@gmail.com>
2013-11-25 15:19:11 -0800

    santizes and displays user messages.

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,19 @@
-// YOUR CODE HERE:
+$(document).ready(function() {
+  $.ajax({
+    url: 'https://api.parse.com/1/classes/chatterbox',
+    success: function(data){
+      // console.log(data.results);
+      messageDisplay(data.results);
+    }
+  });
+
+  var messageDisplay = function(messages) {
+    _.each(messages, function(message) {
+      $('.messages').append('<div>' + sanatize(message.text) + '</div>');
+    });
+  };
+
+  var sanatize = function(string) {
+    return encodeURIComponent(string);
+  };
+});
