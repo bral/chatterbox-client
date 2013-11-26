690edb520c29582c74dc7f0ab68ef428e4796910
brannonlucas <brannonlucas@gmail.com>
2013-11-25 18:15:21 -0800

    diplaying messages and sanatizing output

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,96 @@
-$(document).ready(function() {
+var ChatterBox = function(){
+  this.url = 'https://api.parse.com/1/classes/chatterbox';
+  this.numMessages = 10;
+};
+
+ChatterBox.prototype.getMessages = function() {
+  var that = this;
   $.ajax({
-    url: 'https://api.parse.com/1/classes/chatterbox',
+    url: this.url,
+    data: {limit: this.numMessages, order: '-createdAt'},
+    type: 'GET',
     success: function(data){
-      // console.log(data.results);
-      messageDisplay(data.results);
+      // var messages = that.compareTime(data.results);
+      that.messageDisplay(data.results);
     }
   });
+};
 
-  var messageDisplay = function(messages) {
-    _.each(messages, function(message) {
-      $('.messages').append('<div>' + sanatize(message.text) + '</div>');
-    });
-  };
+ChatterBox.prototype.postMessage = function(username, message, roomname) {
+  var that = this;
 
-  var sanatize = function(string) {
-    return encodeURIComponent(string);
-  };
+  $.ajax({
+    url: this.url,
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({username: username, text: message, roomname: roomname}),
+    success: function(data){
+      console.log('Message posted');
+    }
+  });
+};
+
+ChatterBox.prototype.messageDisplay = function(messages) {
+  var that = this;
+
+  messages = messages.reverse();
+
+  // Get current oldest message
+  // var oldMessage = $('.messages').find('.message')[0];
+  // var oldTimestamp = $(oldMessage).find('.timeStamp').data('timestamp');
+  // console.log(oldMessage);
+
+  var results = [];
+
+  _.each(messages, function(message) {
+
+    // if(oldTimestamp > message.updatedAt) {
+      results.push($('<div class="message">' +
+                      '<span class="username">' +
+                      that.sanatize(message.username) +
+                      '</span> ' +
+                      that.sanatize(message.text) +
+                      ' <span class="timeStamp" data-timestamp="' +
+                      message.updatedAt +
+                      '">' +
+                      moment(message.updatedAt).startOf('hour').fromNow() +
+                      '</span></div>'));
+    // }
+  });
+
+  // console.log(results);
+  console.log('updating');
+  $('.messages').html(results);
+};
+
+ChatterBox.prototype.sanatize = function(string) {
+  return $('<div></div>').text(string).html();
+};
+
+// Returns array of messages that aren't already displayed.
+// ChatterBox.prototype.compareTime = function(messages) {
+//   var oldMessage = $('.messages').find('.message')[0];
+//   var oldTimestamp = $(oldMessage).find('.timeStamp').data('timestamp');
+
+//   if(oldTimestamp === undefined) {
+//     return messages;
+//   } else {
+//     // compare timestamps
+//     for(var i = 0; i < messages.length; i++) {
+//       console.log('Comparing', moment(oldTimestamp).unix(), moment(messages[i].createdAt).unix());
+//       if(moment(oldTimestamp).unix() < moment(messages[i].createdAt).unix()) {
+//         console.log('removed', messages.splice(i, 1));
+//       }
+//     }
+//     return messages;
+//   }
+// };
+
+
+$(document).ready(function() {
+  var chat = new ChatterBox();
+
+  setInterval(function(){
+    chat.getMessages();
+  }, 2000);
 });
